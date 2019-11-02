bf4d31561148cbd5524db9c8648465439108ee00
박선영 <sunsmile01023@gmail.com>
2019-11-02 15:39:28 +0900

    typescript 클래스

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,19 @@
 //블록 세부 설명같은거 정의 
 //interface 는 js 에서 안돌아감 -> 대신 class 쓴다
 
-interface Human{
-    name: string,
-    age: number,
-    gender: string
+class Human{
+    public name: string;
+    public age: number;
+    public gender: string
+
+    constructor(name:string, age:number, gender:string){
+        this.name=name;
+        this.age=age;
+        this.gender=gender;
+    }
 }
 
-const person ={
-    name :"sun",
-    age:27,
-    gender:"female"
-}
+const sun=new Human("sun",20,"female");
 
 
 //sayHi 의 parameter 에 타입을 명시해줌으로써 더 명확하게 코드작성 가능 
@@ -21,6 +23,6 @@ const sayHi = (person: Human): string=> {
     return `Hello ${person.name}, you are ${person.age} and ${person.gender}!`;
 }
 
-console.log(sayHi(person));
+console.log(sayHi(sun));
 
 export {};
