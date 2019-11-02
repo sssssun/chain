73a0ea0494fc3a3cfa3734215776d80d595fb469
박선영 <sunsmile01023@gmail.com>
2019-11-02 16:15:17 +0900

    blockchain 기본 구조, hash 적용

--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,56 @@
 //블록 세부 설명같은거 정의 
 //interface 는 js 에서 안돌아감 -> 대신 class 쓴다
 
-class Human{
-    public name: string;
-    public age: number;
-    public gender: string
+import * as CryptoJs from "crypto-js";
 
-    constructor(name:string, age:number, gender:string){
-        this.name=name;
-        this.age=age;
-        this.gender=gender;
+class Block{
+    public index:number;
+    public hash: string;
+    public previousHash:string;
+    public data:string;
+    public timestamp:number;
+
+    //블록 생성하지 않아도 method 쓸수있도록 하는 것 -> static 
+    static calculateBlockHash=(
+        index: number, previousHash:string, timestamp:number, data:string
+    ):string => CryptoJs.SHA256(index+previousHash+timestamp+data).toString();
+
+    
+
+    constructor(index:number,hash: string,previousHash:string,data:string,timestamp:number){
+        this.index=index;
+        this.hash=hash;
+        this.previousHash=previousHash;
+        this.data=data;
+        this.timestamp=timestamp;
     }
 }
 
-const sun=new Human("sun",20,"female");
+const genesisBlock: Block=new Block(0,"1010010101010","","First Block",123456);
 
+let blockchain: Block[]=[genesisBlock];
 
-//sayHi 의 parameter 에 타입을 명시해줌으로써 더 명확하게 코드작성 가능 
-//return type 도 명시 가능 
-const sayHi = (person: Human): string=> {
-    return `Hello ${person.name}, you are ${person.age} and ${person.gender}!`;
+//(): 뒤에 붙는 거는 return type, => 는 무엇을 return할 것인지
+const getBlockchain= (): Block[]=> blockchain;
+
+const getLatestBlock=(): Block => blockchain[getBlockchain.length-1];
+
+const getNewTimestamp=():number => Math.round(new Date().getTime() /1000);
+
+const createNewBlock=(data:string): Block => {
+    const previousBlock: Block = getLatestBlock();
+    const newIndex:number = previousBlock.index +1 ;
+    const newTimestamp: number=getNewTimestamp();
+    const newHash: string=Block.calculateBlockHash(
+        newIndex,previousBlock.hash,newTimestamp,data
+    );
+
+    const newBlock: Block=new Block(newIndex,newHash,previousBlock.hash,data,newTimestamp);
+
+    return newBlock;
 }
 
-console.log(sayHi(sun));
+console.log(createNewBlock("hello"));
+
 
 export {};
